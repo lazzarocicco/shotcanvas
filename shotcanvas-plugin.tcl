959d75216abf12e8f20c05ae0919f8f499881593
marrongiallo <lazzarocicco@gmail.com>
2020-12-10 20:15:28 +0100

    12-10-20_20:15:28

--- shotcanvas-plugin.tcl
+++ shotcanvas-plugin.tcl
@@ -1,5 +1,5 @@
 # META NAME shotcanvas (shotcanvas-plugin.tcl)
-# META DESCRIPTION load a png as patch background 
+# META DESCRIPTION V02.3 load a png as patch background 
 # META AUTHOR <Lazzaro Ciccolella> lazzarocicco@gmail.com
 
 #    from README file
@@ -16,10 +16,17 @@ package require Img
 puts "- shotcanvas-plugin ---------"
 puts "  pure data (pd) plugin - lazzaro Ciccolella 2020 marrongiallo.github.io"
 puts "  README https://github.com/marrongiallo/shotcanvas"
+puts "  V0.2.3"
 puts "-------------------------"
+
+::pdwindow::post "shotcanvas-plugin V 0.2.3 - pure data (pd) plugin\n"
+::pdwindow::post "lazzaro Ciccolella 2020 marrongiallo.github.io\n"
+::pdwindow::post "README https://github.com/marrongiallo/shotcanvas\n"
+::pdwindow::post "-------------------------\n"
 
 ## shotcanvas-svgcontainer.tcl contain a proc that push the SVG skeleton inside this script 
 source $::current_plugin_loadpath/shotcanvas-svgcontainer.tcl
+
 
 ##if you have gimp or inkscape installed it should open when the image creation is complete
 ##set edit_program gimp
@@ -31,6 +38,8 @@ namespace eval Shotc {
    variable edit_program "inkscape"
 }
 
+
+
 proc crea_svg {mytop image_to_contain file_to_save_svg} {
         set height [winfo height $mytop.c]
         set width [winfo width  $mytop.c]
@@ -41,7 +50,7 @@ proc crea_svg {mytop image_to_contain file_to_save_svg} {
         puts $outputFile $::XML
         ## NON CANCELLARE
         close $outputFile
-        puts "hai creato il file svg: $file_to_save_svg"
+        ::pdwindow::post "$file_to_save_svg saved in $::Shotc::img_folder"
         exec $::Shotc::edit_program $file_to_save_svg  &
 }
 
@@ -53,17 +62,20 @@ proc shot {} {
 	set svg_backup $::Shotc::img_folder/[file root $::windowname($object)]_[clock format [clock seconds] -format "%y-%m-%d_%H_%M_%S"].svg
 		if {[file exists $img_path]} {
 			file copy $img_path $img_backup
+		        ::pdwindow::post "old $img_path copied as $img_backup"
 			image create photo canvasdata -format window -data $object.c
 			canvasdata write $img_path -format png
 			image delete canvasdata
 			} else {
 				image create photo canvasdata -format window -data $object.c
 				canvasdata write $img_path -format png
+			        ::pdwindow::post "$img_path saved in $::Shotc::img_folder"
 				image delete canvasdata
 		}
 
 		if {[file exists $svg_path]} {
 			file copy $svg_path $svg_backup
+		        ::pdwindow::post "old $svg_path copied as $svg_backup"
 			::crea_svg $object $img_path $svg_path
 			} else {
 				::crea_svg $object $img_path $svg_path
@@ -72,7 +84,7 @@ proc shot {} {
 
 proc en_dis_item_menu {state msg} {
 	.menubar.shot entryconfigure 0 -state $state -label $msg
-	puts "$state - $msg"
+	#puts "$state - $msg"
 }
 
 proc update_img {filename rndId} {
@@ -88,7 +100,7 @@ proc create_bg {object imgid} {
 
 bind PatchWindow <<Loaded>>  {+create_bg %W "w[expr int([expr rand()* 50000000])]imgid"}
 bind PdWindow <FocusIn>  {+en_dis_item_menu "disabled" "only for patch"}
-bind PatchWindow <FocusIn>  {+set ::Shotc::current_focused %W;en_dis_item_menu "normal" "crea png"}
+bind PatchWindow <FocusIn>  {+set ::Shotc::current_focused %W;en_dis_item_menu "normal" "crea png";}
 
 set m .menubar
 menu $m.shot
